8e674b20ade3aa138bef4c7420d4fc5a02b397ea
AngryPowman <138001655@qq.com>
2018-09-02 20:33:02 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import AVGStudioMain from "./layouts/AVGStudioMain";
+
+import "./App.scss";
+
+class App extends React.Component {
+  public render() {
+    return <AVGStudioMain />;
+  }
+}
+
+export default App;
---
+++ src/layouts/AVGStudioMain.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class AVGStudioMain extends React.Component {
+  public render() {
+    return <div />;
+  }
+}
+
+export default AVGStudioMain;
